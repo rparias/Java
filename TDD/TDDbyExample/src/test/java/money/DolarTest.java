8054addddda30de6413f073bbe7c725d0c0cb60c
Ronald Arias <ariasron@hotmail.com>
2020-01-26 11:23:00 -0500

    Value Object pattern implemented to use equals in objects

--- TDD/TDDbyExample/src/test/java/money/DolarTest.java
+++ TDD/TDDbyExample/src/test/java/money/DolarTest.java
@@ -1,8 +1,11 @@
 package money;
 
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 public class DolarTest {
@@ -10,7 +13,15 @@ public class DolarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertThat(five.amount, is(10));
+        Dollar product = five.times(2);
+        assertThat(product.amount, is(10));
+        product = five.times(3);
+        assertThat(product.amount, is(15));
+    }
+
+    @Test
+    public void testEquality() {
+        assertThat(new Dollar(5), is(equalTo(new Dollar(5))));
+        assertThat(new Dollar(5), is(not(equalTo(new Dollar(6)))));
     }
 }
