4c3fac33404c5ff272d401f291ceb87836b0b4dd
Ronald Arias <ariasron@hotmail.com>
2020-02-03 21:59:17 -0500

    returning Money instead of Franc

--- TDD/TDDbyExample/src/main/java/money/Dollar.java
+++ TDD/TDDbyExample/src/main/java/money/Dollar.java
@@ -5,9 +5,4 @@ public class Dollar extends Money {
     public Dollar(int amount, String currency) {
         super(amount, currency);
     }
-
-    @Override
-    public Money times(int multiplier) {
-        return new Dollar(amount * multiplier, "USD");
-    }
 }
--- TDD/TDDbyExample/src/main/java/money/Money.java
+++ TDD/TDDbyExample/src/main/java/money/Money.java
@@ -5,10 +5,6 @@ public class Money {
     protected int amount;
     protected String currency;
 
-    Money times(int multiplier) {
-        return null;
-    }
-
     Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
@@ -18,17 +14,26 @@ public class Money {
         return currency;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        Money money = (Money) obj;
-        return amount == money.amount && getClass().equals(money.getClass());
-    }
-
     static Money dollar(int amount) {
         return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
         return new Franc(amount, "CHF");
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 }
